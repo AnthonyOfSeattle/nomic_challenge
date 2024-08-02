03d95a83dc7ff5dd95ad546a250f300128915539
Anthony Valente <valenta4@uw.edu>
2024-08-02 12:41:08 -0400

    Add models to admin site

--- app/plates/admin.py
+++ app/plates/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from plates.models import AnalysisResult, BeadPlate, Plate, Run
 
-# Register your models here.
+
+@admin.register(AnalysisResult)
+class AnalysisResultAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BeadPlate)
+class BeadPlateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Plate)
+class PlateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Run)
+class RunAdmin(admin.ModelAdmin):
+    pass
+
